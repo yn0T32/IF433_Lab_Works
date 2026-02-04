84b3c8164815272ee0296e2be1e25d1d6ed3be21
yno0T <chandraantony87@gmail.com>
2026-02-04 11:26:43 -0500

    week01: (tugas 2) implement receipt printing

--- src/oop_00000124960_AntonyChandra/week01/GameStore.kt
+++ src/oop_00000124960_AntonyChandra/week01/GameStore.kt
@@ -3,9 +3,17 @@ package oop_00000124960_AntonyChandra.week01
 fun main () {
     val gameTitle = "Metal Gear Solid V: Phantom Pain"
     val price = 672000
+    printReceipt(gameTitle, price)
 }
 
-fun calculatDiscount(price: Int): Int = (if (price > 500000) price * 20 else price * 100)
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) (price * 0.8).toInt() else price
+
+fun printReceipt(title: String, price: Int) {
+    val finalPrice = calculateDiscount(price)
+    println("Game: $title")
+    println("Final Price: Rp$finalPrice")
+}
 
 
 
