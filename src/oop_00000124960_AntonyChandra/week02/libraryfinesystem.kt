f3101db137705d823db05c018348c040929acf5f
yno0T <chandraantony87@gmail.com>
2026-02-11 05:46:39 -0500

    week02: (task 1) implement library input system

--- src/oop_00000124960_AntonyChandra/week02/libraryfinesystem.kt
+++ src/oop_00000124960_AntonyChandra/week02/libraryfinesystem.kt
@@ -5,23 +5,28 @@ class loan (var booktitle: String, var borrower: String, var loanDuration: Int){
     init {
         loanDuration = 1;
     }
-    fun calculateFine(){
-        if (loanDuration  >= 3){
-            var totalDenda = loanDuration - 3 * 2000
+    fun calculateFine(): Int{
+        var totalDenda: Int
+        if (loanDuration >= 3){
+            totalDenda = (loanDuration - 3) * 2000
         } else {
-            var totalDenda = loanDuration - 3
+            totalDenda = loanDuration - 3
         }
+        return totalDenda
     }
-    fun main(){
-        val scanner = Scanner(System.`in`)
-        println("Judul Buku: ")
-        booktitle=scanner.nextLine()
+}
+fun main(){
+    var pinjem = loan("", "", 1,)
+    val scanner = Scanner(System.`in`)
+    println("Judul Buku: ")
+    pinjem.booktitle=scanner.nextLine()
 
-        println("Masukkan nama Peminjam: ")
-        borrower=scanner.nextLine()
+    println("Masukkan nama Peminjam: ")
+    pinjem.borrower=scanner.nextLine()
 
-        println("Pinjam berapa hari? (tidak boleh minus!)")
-        loanDuration=scanner.nextInt()
+    println("Pinjam berapa hari? (tidak boleh minus!)")
+    pinjem.loanDuration=scanner.nextInt()
 
-    }
+    val denda = pinjem.calculateFine()
+    println("Judul Buku: ${pinjem.booktitle}\nNama Peminjam: ${pinjem.borrower}\nLoan Duration: ${pinjem.loanDuration}\nTotal Denda: Rp $denda")
 }
