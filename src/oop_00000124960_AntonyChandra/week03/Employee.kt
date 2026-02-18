1ef8f3791a21b8d17940121af9e4205e97952ec5
yno0T <chandraantony87@gmail.com>
2026-02-18 03:33:53 -0500

    week03: simulate stack overflow error in setter

--- src/oop_00000124960_AntonyChandra/week03/Employee.kt
+++ src/oop_00000124960_AntonyChandra/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_00000124960_AntonyChandra.week03
 
 class Employee(name:String) {
     var salary: Int = 0
+    set(value) {
+        println("Mencoba set gaji ke: $value")
+        this.salary = value
+    }
 }
---
+++ src/oop_00000124960_AntonyChandra/week03/main.kt
@@ -0,0 +1,7 @@
+package oop_00000124960_AntonyChandra.week03
+
+fun main(){
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
