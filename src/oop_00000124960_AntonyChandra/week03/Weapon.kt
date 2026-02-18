dedcf75297ac2171de3fe151c218d536342fac2e
yno0T <chandraantony87@gmail.com>
2026-02-18 06:55:24 -0500

    week03: (task 1) implement Weapon testin in main

--- src/oop_00000124960_AntonyChandra/week03/Weapon.kt
+++ src/oop_00000124960_AntonyChandra/week03/Weapon.kt
@@ -1,26 +1,35 @@
 package oop_00000124960_AntonyChandra.week03
 
 class Weapon(){
-    private var name: String = "Tony"
-    var damage: Int = 2
+    private var name: String = "Excalibur"
+    public var damage: Int = 2
     set(value) {
         if (value < 0) {
             println("Damage can't be negative")
+            field = damage
         } else if (value > 1000) {
             println("Damage can't be greater than 1000 ")
-            damage = 1000
+            field = 1000
         } else {
-            println("Damage is $value")
+            field = value
         }
     }
-    var tier: String? = null
-    set(value) {
-        if(damage > 800){
-            println("The tier is Legendary")
-        } else if (damage > 500){
-            println("The tier is Epic")
-        } else {
-            println("The tier is Common")
+        get(){
+            return field
         }
+
+    fun get_damage(): Int {
+        return damage
+    }
+    private var tier: String = "Common"
+        get() {
+            return when {
+                damage > 800 -> "Legendary"
+                damage > 500 -> "Epic"
+                else -> "Common"
+            }
+        }
+    fun get_tier(): String{
+        return tier
     }
 }
---
+++ src/oop_00000124960_AntonyChandra/week03/mainWeapon.kt
@@ -0,0 +1,7 @@
+package oop_00000124960_AntonyChandra.week03
+
+fun main(){
+    var Senjata = Weapon()
+    Senjata.damage = 600
+    println("Damage: ${Senjata.get_damage()}\nTier: ${Senjata.get_tier()}")
+}
