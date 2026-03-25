1b8c1fefe3bfcdead702d7ab1e923bd33c13cf7b
yno0T <chandraantony87@gmail.com>
2026-03-25 16:31:52 +0700

    Week07: (task) create ItemRarity enum

---
+++ src/oop_00000124960_AntonyChandra/week7/TugasMandiri/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000124960_AntonyChandra.week7.TugasMandiri
+
+enum class ItemRarity (val DropChance: Int) {
+    COMMON(70),
+    UNCOMMON(60),
+    RARE(50),
+    EPIC(20),
+    LEGENDARY(1)
+}
